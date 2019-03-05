e3eff0baab5a02486730a8fc79240f0d8abe1f18
Tristan Sloughter <t@crashfast.com>
2019-03-05 11:56:12 -0700

    upgrade to opencensus_elixir 0.3 which uses opencensus 0.9+

--- mix.exs
+++ mix.exs
@@ -4,10 +4,22 @@ defmodule OpencensusTesla.MixProject do
   def project do
     [
       app: :opencensus_tesla,
-      version: "0.1.0",
-      elixir: "~> 1.8-dev",
+      version: "0.2.0",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      description: description(),
+      package: package(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test,
+        docs: :docs,
+        "inchci.add": :docs,
+        "inch.report": :docs
+      ]
     ]
   end
 
@@ -17,10 +29,41 @@ defmodule OpencensusTesla.MixProject do
     ]
   end
 
+  defp aliases do
+    [
+      test: "test --no-start"
+    ]
+  end
+
   defp deps do
     [
       {:tesla, "~> 1.2"},
-      {:opencensus_elixir, github: "opencensus-beam/opencensus_elixir"}
+      {:opencensus_elixir, "~> 0.3"},
+
+      # Documentation
+      {:ex_doc, ">= 0.0.0", only: [:docs]},
+      {:inch_ex, "~> 1.0", only: [:docs]},
+
+      # Testing
+      {:excoveralls, "~> 0.10.3", only: [:test]},
+      {:dialyxir, ">= 0.0.0", runtime: false, only: [:dev, :test]},
+      {:junit_formatter, ">= 0.0.0", only: [:test]}
+    ]
+  end
+
+  defp description() do
+    "Tesla middleware for OpenCensus tracing"
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/opencensus-beam/opencensus_tesla",
+        "OpenCensus" => "https://opencensus.io",
+        "OpenCensus Erlang" => "https://github.com/census-instrumentation/opencensus-erlang",
+        "OpenCensus BEAM" => "https://github.com/opencensus-beam"
+      }
     ]
   end
 end

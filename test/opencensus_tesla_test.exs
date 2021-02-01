80033ab68d5e71a56e2b64ce897b463f25ed39d3
Piotr Dobaczewski <5735525+pdobacz@users.noreply.github.com>
2021-02-01 13:04:57 +0100

    add a simple test for the override

--- test/opencensus_tesla_test.exs
+++ test/opencensus_tesla_test.exs
@@ -14,10 +14,26 @@ defmodule TestRequestClient do
   end)
 end
 
+defmodule TestRequestClientCustomized do
+  use Tesla
+
+  plug(OpencensusTesla.Middleware, span_name_override: fn path -> path <> "test" end)
+
+  adapter(fn env ->
+    {status, headers, body} =
+      case env.url do
+        _ ->
+          {200, [], ""}
+      end
+
+    {:ok, %{env | status: status, headers: headers, body: body}}
+  end)
+end
+
 defmodule OpencensusTeslaTest do
   use ExUnit.Case
 
-  test "span is reported after request is complete" do
+  setup do
     Application.load(:opencensus)
     Application.put_env(:opencensus, :send_interval_ms, 1)
     Application.put_env(:opencensus, :reporters, [{:oc_reporter_pid, self()}])
@@ -25,9 +41,21 @@ defmodule OpencensusTeslaTest do
     {:ok, _} = Application.ensure_all_started(:opencensus)
     {:ok, _} = Application.ensure_all_started(:opencensus_elixir)
 
+    on_exit(fn -> Application.stop(:opencensus) end)
+    :ok
+  end
+
+  test "span is reported after request is complete" do
     {:ok, env} = TestRequestClient.post("/", "")
     assert 200 = env.status
 
-    assert_receive {:span, _}, 1_000
+    assert_receive {:span, {:span, "/", _, _, _, _, _, _, _, _, _, _, _, _, _, _, _}}, 1_000
+  end
+
+  test "can override span name" do
+    {:ok, env} = TestRequestClientCustomized.post("/", "")
+    assert 200 = env.status
+
+    assert_receive {:span, {:span, "/test", _, _, _, _, _, _, _, _, _, _, _, _, _, _, _}}, 1_000
   end
 end

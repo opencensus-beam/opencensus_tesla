c684d6d466a04dfbc7d92da1a6686566d197046d
Piotr Dobaczewski <5735525+pdobacz@users.noreply.github.com>
2021-02-01 12:53:04 +0100

    make span_name_override a function argument

--- lib/opencensus_tesla.ex
+++ lib/opencensus_tesla.ex
@@ -22,13 +22,16 @@ defmodule OpencensusTesla.Middleware do
   Following Middleware options are accepted:
     - `span_name_override` - if provided, will be used as the
       span name. Otherwise request path is used by default.
+      A function taking the request path as argument and
+      returning a string to use as span name is expected.
   """
 
   import Opencensus.Trace
 
   def call(env, next, options) do
     uri = %URI{path: path} = URI.parse(env.url)
-    span_name = Keyword.get(options, :span_name_override, path)
+    span_name_function = Keyword.get(options, :span_name_override)
+    span_name = if span_name_function, do: span_name_function.(path), else: path
 
     with_child_span(span_name, http_attributes(env, uri)) do
       span_ctx = :ocp.current_span_ctx()
